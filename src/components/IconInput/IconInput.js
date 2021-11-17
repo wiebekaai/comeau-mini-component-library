321a7070046903991ed7304b492a2ef69956c899
Wiebe Kaai <wiebekaai@gmail.com>
2021-11-17 14:30:48 +0100

    Icon input

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -13,7 +13,74 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  const SIZE_STYLES = {
+    small: {
+      height: 24,
+      fontSize: 14,
+      iconSize: 16,
+      borderWidth: 1,
+    },
+    large: {
+      height: 36,
+      fontSize: 18,
+      iconSize: 24,
+      borderWidth: 2,
+    },
+  };
+
+  const styles = SIZE_STYLES[size];
+
+  return <Wrapper>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <IconWrapper>
+          <Icon id={icon} size={styles.iconSize} strokeWidth="2" />
+      </IconWrapper>
+      <Input 
+        placeholder={placeholder}
+        style={{
+          '--width': width,
+          '--height': styles.height / 16 + 'rem',
+          '--border-width': styles.borderWidth + 'px',
+          '--font-size': styles.fontSize / 16 + 'rem'
+        }}
+      />
+  </Wrapper>;
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: fit-content;
+  color: ${COLORS.gray500};
+
+  &:hover {
+    color: ${COLORS.black}
+  }
+`;
+
+const Input = styled.input`
+  font-size: var(--font-size);
+  border: none;
+  width: var(--width);
+  height: var(--height);
+  outline-offset: 2px;
+  padding-left: var(--height);
+  font-weight: bold;
+  color: inherit;
+  border-bottom: var(--border-width) solid ${COLORS.black};
+
+  &::placeholder {
+    font-weight: normal;
+    color: ${COLORS.gray700};
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  left: 0;
+  top: 0;
+  display: flex;
+  height: 90%;
+  align-items: center;
+`;
 
 export default IconInput;
